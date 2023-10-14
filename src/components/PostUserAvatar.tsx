6f8e9b7211ff52ba7b4d500bcbdc334e28c5d3fc
hyunwoo93.cha <hyunwoo93.cha@lgepartners.com>
2023-10-14 23:09:03 +0900

    사용자 닉네임 클릭 시 화면 전환

--- src/components/PostUserAvatar.tsx
+++ src/components/PostUserAvatar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Avatar from "./Avatar";
 
 interface IProps {
@@ -9,7 +10,9 @@ export default function PostUserAvatar({ image, username }: IProps) {
   return (
     <div className="flex items-center p-2">
       <Avatar image={image} highlight size="medium" />
-      <span className="text-gray-900 font-bold ml-2">{username}</span>
+      <Link href={`/user/${username}`} passHref>
+        <span className="text-gray-900 font-bold ml-2">{username}</span>
+      </Link>
     </div>
   );
 }
